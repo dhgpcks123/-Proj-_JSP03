f0beec1899962a0b2a7e7c246020281ef4a1a1e7
A <A@211.204.60.111>
2020-11-23 18:22:54 +0900

    js 3차

--- Team03Proj/WebContent/WEB-INF/resources/js/memberjoin.js
+++ Team03Proj/WebContent/WEB-INF/resources/js/memberjoin.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
-    $("#chk_all").prop("checked",false);
+    /* 약관동의 진입시 체크해제  */
+	$("#chk_all").prop("checked",false);
 		setTerms();
 
-    $("#chk_all").click(function() {
-        location.hash = 'agreeBottom';
+    /* 약관동의 체크 모두동의 */
+    $("#chk_all").click(function(){
         setTerms();
-    })	
+    });	
  	
+    /* 약관동의 모두 체크(true), 체크해제(false) 기능*/
 	function setTerms() {
         if ($("#chk_all").is(":checked")) {
             $("#termsService").prop("checked",true);
@@ -17,11 +19,10 @@ $(document).ready(function(){
             $("#termsPrivacy").prop("checked",false);
             $("#termsLocation").prop("checked",false);
         }
-
         return true;
     }
-   
 
+    /* 약관동의 필수 체크(true) 확인 기능*/
     function checkTerms() {
         var res = true;
 
@@ -36,6 +37,7 @@ $(document).ready(function(){
         return res;
     }
 
+    /* 약관동의 필수 체크(true) 전송 기능*/
     function submitAgree() {
         if (checkTerms() != true) {
             return false;
@@ -45,10 +47,75 @@ $(document).ready(function(){
         return true;
     }
 
+    /* 약관동의 버튼(Send) 기능*/
     $("#btnAgree").click(function() {
 		checkTerms();
         submitAgree();
         return false;
-    })
+    });
+	
+/*	/* 입력확인 
+	$('.info').ready(function(){
+		var sid = $('#id').focusout();
+		var spw = $('#pw').focusout();
+			
+		if(!sid || !spw){
+			$('#idmsg').html('필수정보입니다.');
+			$('#idmsg').addClass('w3-text-red');
+			$('#idmsg').stop().show(500);
+			return;
+		}
+		
+	});*/
+	
+    //region unreal id
+    $("#id").blur(function() {
+        checkId();
+    });
 
+ 	function checkId() {
+        var id = $("#id").val();
+        var oMsg = $("#idMsg");
+        var oInput = $("#id");
+
+        if ( id == "") {
+            showErrorMsg(oMsg,"필수 정보입니다.");
+            return false;
+        }
+	}
+
+    function showErrorMsg(obj, msg) {
+        obj.attr("class", "info");
+        obj.html(msg);
+        obj.show();
+    }
+
+
+
+
+
+	/* 회원가입 step1 버튼 */
+	$('#next1').click(function(){
+		$('#step1').addClass('w3-hide');
+		$('#step2').removeClass('w3-hide');
+	});
+	/* 회원가입 step2버튼 */
+	$('#pre2').click(function(){
+		$('#step1').removeClass('w3-hide');
+		$('#step2').addClass('w3-hide');
+	});
+	$('#next2').click(function(){
+		$('#step2').addClass('w3-hide');
+		$('#step3').removeClass('w3-hide');
+	});
+	/* 회원가입 step3 버튼 */
+	$('#pre3').click(function(){
+		$('#step2').removeClass('w3-hide');
+		$('#step3').addClass('w3-hide');
+	});
+	$('#next3').click(function(){
+		$('#step2').addClass('w3-hide');
+		$('#step3').removeClass('w3-hide');
+	});
+	
 });
---
+++ Team03Proj/WebContent/WEB-INF/views/com/Team03Proj/www/member/MemberInfoView.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet" type="text/css" href="/Team03Proj/css/w3.css">
+<link rel="stylesheet" type="text/css" href="/Team03Proj/css/member/member.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script type="text/javascript" src="/Team03Proj/js/jquery-3.5.1.min.js"></script>
+<script type="text/javascript" src="/Team03Proj/js/memberjoin.js"></script>
+</head>
+<body>
+	<div></div>
+</body>
+</html>
